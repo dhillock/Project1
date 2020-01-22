cc73487b508f35b0e30c466cd0a8283d77aca12a
David Hillock <dhillock@rogers.com>
2020-01-22 08:29:00 -0500

    Feat: Added more styling

---
+++ js/main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("working");
+
+  let cartButtonClicks = 0;
+  const cartButtonClicked = document.getElementsByClassName("cartButton");
+  //   console.log(cartButtonClicked);
+
+  //   cartButtonClicked.addEventListener("click", function() {
+  //     cartButtonClicks++;
+  //     console.log(cartButtonClicks);
+  //   });
+});
