a8908dd2b739b76e35178ebed6c6cc337274b5af
David Hillock <dhMacPro61@Davids-Mac-Pro-114.local>
2020-01-24 00:32:51 -0500

    Feat: Added mouse capture on menuMain

--- js/main.js
+++ js/main.js
@@ -18,3 +18,32 @@ document.addEventListener("DOMContentLoaded", function() {
     // options
   });
 });
+
+/*xxxxxxxxxxxxxxxxxx*/
+
+// document.addEventListener(
+//   "click",
+//   function(event) {
+//     // If the clicked element doesn't have the right selector, don't continue
+//     if (!event.target.matches(".menuMain")) return;
+
+//     // Log the clicked element in the console
+//     console.log("You clicked the menuMain");
+//     console.log(event.target.innerHTML);
+//     // console.log(event.innerHTML);
+//     console.log(event.target);
+//   },
+//   false
+// );
+
+var listItems = document.querySelectorAll("ul li"); // this returns an array of each li
+listItems.forEach(function(item) {
+  item.onclick = function(e) {
+    console.log(this.innerText); // this displays clicked li's value
+
+    if (this.innerText == "Updates") {
+      var elmnt = document.getElementById("jumpUpdates");
+      elmnt.scrollIntoView();
+    }
+  };
+});
