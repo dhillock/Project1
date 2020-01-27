e6244dd489370597bc19680b3613fe46f77cbcf9
David Hillock <dhMacPro61@Davids-Mac-Pro-114.local>
2020-01-26 22:18:21 -0500

    Style: Completed styles for all device formats.

--- README.md
+++ README.md
@@ -1,4 +1,72 @@
-# Aloha Project 1
+<H1 style='color:yellow'>The Aloha Apparel Company Website - Project 1</H1>
 
+<H2 style='color:yellow'>Brief Description:</H2>
 
-This is my Aloha project
+This is the first required project from RED Academy, in the **Web Development Program**.
+The purpose of the project is to design a responsive home page.
+More specifically, a page that would show "properly" in \* _mobile, tablet, and desktop_ \* environments.
+
+<H2 style='color:yellow'>Technology Used:</H2>
+
+This project uses the following technology:
+
+- HTML
+- CSS
+- Javascript
+- Fonts are from Font Awesome
+- A carousel plugin from Flickity
+- GitHub
+
+Development tools include:
+
+- Visual Code Studio
+
+  ```
+       - Bracket Pair Colorizer
+       - GitLens
+       - Live Server
+       - Live Share
+       - Path Autocomplete
+       - Prettier - Code Formatter
+  ```
+
+- iTerm2
+
+<H2 style='color:yellow'>Personal Learnings:</H2>
+
+What I learned in the last two weeks, seems like A LOT. However, when I step back, I realize that what I learned could be fit into a grain of sand that is sitting on a beach. There is SO MUCH more to learn.
+
+While it’s true that I learned the very basics about HTML, CSS, and JavaScript, I also learned the basics related to the development environment – Visual Studio Code, and related plugins; iTerm2; GitHub, etc.
+
+I learned now how the program works – lessons in the morning, and practice in the afternoon, with other stuff – like Professional Development – is injected throughout the week.
+
+I’ve learned that the answer to any question I have, has probably been asked by someone else, and therefore, can be found on the Internet. It’s just a matter of digging.
+
+I’ve learned that the Problem-Solving Steps that were outlined on day-1 are important, and for a couple of reasons. First, you won’t be able to survive in the work environment, if you are always asking your supervisor/boss/colleagues things that you should be able to find out yourself. But I’ve also learned that when you are learning green-field stuff, you don’t often know what questions to ask, and that can waste/absorb A LOT of time.
+
+With regards to the actual project, I’ve learned that there are a million ways to do something, but most likely, there are two or three right ways. On a scale of 1-10, I give myself 60 percent. While I’m sure that I got many things right, my CSS coding would have been more dynamic or responsive. I need to learn more about using percentages, the dynamics of the viewport, and inheritance. I’m sure that my code for this project can be reduced by 30 percent, and still accomplish the goals.
+
+More specifically, I’m pretty comfortable now with HTML and if I have an issue or problem, I’m confident that I can solve it quickly, and correctly. I have a lot more to learn about CSS. I’m understanding the basics of JavaScript, and my sense is that the key to success is to know JavaScript A LOT more.
+
+Even though the weekdays at schools are intense, and it’s like drinking from a firehose all day…every day, next week will be a tad more familiar with the week before.
+
+My most productive days – in terms of output and learning – was when I organized the team meetings with John and Johnny. We have something going now, so I’m looking forward to leveraging that more. The three of us talked about adding someone new to our team. I’m looking forward to that.
+
+<H2 style='color:yellow'>Install Information:</H2>
+
+All necessary files are in the Project 1 folder, using the following folder structure:
+
+- .git
+- .vscode
+- .gitignore
+- assets
+  - fonts
+  - images
+- css
+- fonts
+- images
+- js
+- node_modules
+  - prettier
+
+**_ End of ReadMe.md _**
--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
+  // Do not reanme the 3 flickity vars here, or the flickty carousel will break.
   var elem = document.querySelector(".carousel");
   var flkty = new Flickity(elem, {
     // options
     cellAlign: "left",
     contain: true,
-    freeScroll: true,
+    freeScroll: false,
     wrapAround: true
   });
   // element argument can be a selector string
@@ -13,39 +14,55 @@ document.addEventListener("DOMContentLoaded", function() {
     // options
   });
 
-  // identify the submitButton, and letup a listener
+  // Identify the submitButton, and setup a listener
   document
     .getElementById("submitButton")
     .addEventListener("click", function(event) {
-      // alert("Thank you for Subscribing !");
-      // pull out from the test box, the email address
       event.preventDefault();
 
-      let tstVar = document.getElementById("emInput");
-      // console.log(tstVar);
-      if (validateEmail(tstVar.value)) {
-        alert("Thank you for subscribing");
-        tstVar.value = "";
+      // extract the entered email address
+      let enteredEmail = document.getElementById("emInput");
+      // console.log(enteredEmail);
+      if (validateEmail(enteredEmail.value)) {
+        alert("Thank you for subscribing.");
+        enteredEmail.value = "";
       } else {
+        beep();
         alert("Invalid emal address. Please try again.");
+        // do not clear the email address here, so that the user can edit it, and re-submit.
       }
     });
 
+  // Identify which item from all unordered lists was clicked, and react accordingly
+  const listItems = document.querySelectorAll("ul li"); // return an array for each li in the DOM
+  listItems.forEach(function(item) {
+    item.onclick = function(e) {
+      // console.log(this.innerText); // display the clicked li value
+      if (this.innerText === "About") {
+        const elmnt = document.getElementById("jumpAbout");
+        elmnt.scrollIntoView();
+      } else if (this.innerText === "Shop") {
+        const elmnt = document.getElementById("jumpShopGrid");
+        elmnt.scrollIntoView();
+      } else if (this.innerText === "Featured") {
+        const elmnt = document.getElementById("jumpFeatured");
+        elmnt.scrollIntoView();
+      } else if (this.innerText === "Updates") {
+        const elmnt = document.getElementById("jumpUpdates");
+        elmnt.scrollIntoView();
+      }
+    };
+  });
+
+  // Utility functions
   function validateEmail(email) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
   }
 
-  // identify which item from all unordered lists, was clicked, and react accordingly
-  var listItems = document.querySelectorAll("ul li"); // this returns an array of each li in the DOM
-  listItems.forEach(function(item) {
-    item.onclick = function(e) {
-      console.log(this.innerText); // this displays clicked li's value
-
-      if (this.innerText == "Updates") {
-        var elmnt = document.getElementById("jumpUpdates");
-        elmnt.scrollIntoView();
-      }
-    };
-  });
+  function beep() {
+    new Audio(
+      "data:audio/wav;base64,//uQRAAAAWMSLwUIYAAsYkXgoQwAEaYLWfkWgAI0wWs/ItAAAGDgYtAgAyN+QWaAAihwMWm4G8QQRDiMcCBcH3Cc+CDv/7xA4Tvh9Rz/y8QADBwMWgQAZG/ILNAARQ4GLTcDeIIIhxGOBAuD7hOfBB3/94gcJ3w+o5/5eIAIAAAVwWgQAVQ2ORaIQwEMAJiDg95G4nQL7mQVWI6GwRcfsZAcsKkJvxgxEjzFUgfHoSQ9Qq7KNwqHwuB13MA4a1q/DmBrHgPcmjiGoh//EwC5nGPEmS4RcfkVKOhJf+WOgoxJclFz3kgn//dBA+ya1GhurNn8zb//9NNutNuhz31f////9vt///z+IdAEAAAK4LQIAKobHItEIYCGAExBwe8jcToF9zIKrEdDYIuP2MgOWFSE34wYiR5iqQPj0JIeoVdlG4VD4XA67mAcNa1fhzA1jwHuTRxDUQ//iYBczjHiTJcIuPyKlHQkv/LHQUYkuSi57yQT//uggfZNajQ3Vmz+ Zt//+mm3Wm3Q576v////+32///5/EOgAAADVghQAAAAA//uQZAUAB1WI0PZugAAAAAoQwAAAEk3nRd2qAAAAACiDgAAAAAAABCqEEQRLCgwpBGMlJkIz8jKhGvj4k6jzRnqasNKIeoh5gI7BJaC1A1AoNBjJgbyApVS4IDlZgDU5WUAxEKDNmmALHzZp0Fkz1FMTmGFl1FMEyodIavcCAUHDWrKAIA4aa2oCgILEBupZgHvAhEBcZ6joQBxS76AgccrFlczBvKLC0QI2cBoCFvfTDAo7eoOQInqDPBtvrDEZBNYN5xwNwxQRfw8ZQ5wQVLvO8OYU+mHvFLlDh05Mdg7BT6YrRPpCBznMB2r//xKJjyyOh+cImr2/4doscwD6neZjuZR4AgAABYAAAABy1xcdQtxYBYYZdifkUDgzzXaXn98Z0oi9ILU5mBjFANmRwlVJ3/6jYDAmxaiDG3/6xjQQCCKkRb/6kg/wW+kSJ5//rLobkLSiKmqP/0ikJuDaSaSf/6JiLYLEYnW/+kXg1WRVJL/9EmQ1YZIsv/6Qzwy5qk7/+tEU0nkls3/zIUMPKNX/6yZLf+kFgAfgGyLFAUwY//uQZAUABcd5UiNPVXAAAApAAAAAE0VZQKw9ISAAACgAAAAAVQIygIElVrFkBS+Jhi+EAuu+lKAkYUEIsmEAEoMeDmCETMvfSHTGkF5RWH7kz/ESHWPAq/kcCRhqBtMdokPdM7vil7RG98A2sc7zO6ZvTdM7pmOUAZTnJW+NXxqmd41dqJ6mLTXxrPpnV8avaIf5SvL7pndPvPpndJR9Kuu8fePvuiuhorgWjp7Mf/PRjxcFCPDkW31srioCExivv9lcwKEaHsf/7ow2Fl1T/9RkXgEhYElAoCLFtMArxwivDJJ+bR1HTKJdlEoTELCIqgEwVGSQ+hIm0NbK8WXcTEI0UPoa2NbG4y2K00JEWbZavJXkYaqo9CRHS55FcZTjKEk3NKoCYUnSQ 0rWxrZbFKbKIhOKPZe1cJKzZSaQrIyULHDZmV5K4xySsDRKWOruanGtjLJXFEmwaIbDLX0hIPBUQPVFVkQkDoUNfSoDgQGKPekoxeGzA4DUvnn4bxzcZrtJyipKfPNy5w+9lnXwgqsiyHNeSVpemw4bWb9psYeq//uQZBoABQt4yMVxYAIAAAkQoAAAHvYpL5m6AAgAACXDAAAAD59jblTirQe9upFsmZbpMudy7Lz1X1DYsxOOSWpfPqNX2WqktK0DMvuGwlbNj44TleLPQ+Gsfb+GOWOKJoIrWb3cIMeeON6lz2umTqMXV8Mj30yWPpjoSa9ujK8SyeJP5y5mOW1D6hvLepeveEAEDo0mgCRClOEgANv3B9a6fikgUSu/DmAMATrGx7nng5p5iimPNZsfQLYB2sDLIkzRKZOHGAaUyDcpFBSLG9MCQALgAIgQs2YunOszLSAyQYPVC2YdGGeHD2dTdJk1pAHGAWDjnkcLKFymS3RQZTInzySoBwMG0QueC3gMsCEYxUqlrcxK6k1LQQcsmyYeQPdC2YfuGPASCBkcVMQQqpVJshui1tkXQJQV0OXGAZMXSOEEBRirXbVRQW7ugq7IM7rPWSZyDlM3IuNEkxzCOJ0ny2ThNkyRai1b6ev//3dzNGzNb//4uAvHT5sURcZCFcuKLhOFs8mLAAEAt4UWAAIABAAAAAB4qbHo0tIjVkUU//uQZAwABfSFz3ZqQAAAAAngwAAAE1HjMp2qAAAAACZDgAAAD5UkTE1UgZEUExqYynN1qZvqIOREEFmBcJQkwdxiFtw0qEOkGYfRDifBui9MQg4QAHAqWtAWHoCxu1Yf4VfWLPIM2mHDFsbQEVGwyqQoQcwnfHeIkNt9YnkiaS1oizycqJrx4KOQjahZxWbcZgztj2c49nKmkId44S71j0c8eV9yDK6uPRzx5X18eDvjvQ6yKo9ZSS6l//8elePK/Lf//IInrOF/FvDoADYAGBMGb7 FtErm5MXMlmPAJQVgWta7Zx2go+8xJ0UiCb8LHHdftWyLJE0QIAIsI+UbXu67dZMjmgDGCGl1H+vpF4NSDckSIkk7Vd+sxEhBQMRU8j/12UIRhzSaUdQ+rQU5kGeFxm+hb1oh6pWWmv3uvmReDl0UnvtapVaIzo1jZbf/pD6ElLqSX+rUmOQNpJFa/r+sa4e/pBlAABoAAAAA3CUgShLdGIxsY7AUABPRrgCABdDuQ5GC7DqPQCgbbJUAoRSUj+NIEig0YfyWUho1VBBBA//uQZB4ABZx5zfMakeAAAAmwAAAAF5F3P0w9GtAAACfAAAAAwLhMDmAYWMgVEG1U0FIGCBgXBXAtfMH10000EEEEEECUBYln03TTTdNBDZopopYvrTTdNa325mImNg3TTPV9q3pmY0xoO6bv3r00y+IDGid/9aaaZTGMuj9mpu9Mpio1dXrr5HERTZSmqU36A3CumzN/9Robv/Xx4v9ijkSRSNLQhAWumap82WRSBUqXStV/YcS+XVLnSS+WLDroqArFkMEsAS+eWmrUzrO0oEmE40RlMZ5+ODIkAyKAGUwZ3mVKmcamcJnMW26MRPgUw6j+LkhyHGVGYjSUUKNpuJUQoOIAyDvEyG8S5yfK6dhZc0Tx1KI/gviKL6qvvFs1+bWtaz58uUNnryq6kt5RzOCkPWlVqVX2a/EEBUdU1KrXLf40GoiiFXK///qpoiDXrOgqDR38JB0bw7SoL+ZB9o1RCkQjQ2CBYZKd/+VJxZRRZlqSkKiws0WFxUyCwsKiMy7hUVFhIaCrNQsKkTIsLivwKKigsj8XYlwt/WKi2N4d//uQRCSAAjURNIHpMZBGYiaQPSYyAAABLAAAAAAAACWAAAAApUF/Mg+0aohSIRobBAsMlO//Kk4soosy1JSFRYWaLC4qZBYWFRGZdwqKiwkNBVmoWFSJkWFxX4FFRQWR+LsS4W/rFRb//////////////////////////// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////VEFHAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAU291bmRib3kuZGUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMjAwNGh0dHA6Ly93d3cuc291bmRib3kuZGUAAAAAAAAAACU="
+    ).play();
+  }
 });
